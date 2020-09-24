11eef68acbe2d5d7c7793513a07414e940b8738e
DavidKim <zkffhtm6523@naver.com>
2020-09-25 01:07:41 +0900

    2020.09.25 탑툰 크롤링 완료

--- JSP_TeamProject_Team/src/com/allWebtoon/crawling/Comico.java
+++ JSP_TeamProject_Team/src/com/allWebtoon/crawling/Comico.java
@@ -1,37 +1,58 @@
 package com.allWebtoon.crawling;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.io.UnsupportedEncodingException;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.net.URLEncoder;
 import java.util.ArrayList;
-
-import org.json.simple.JSONObject;
-import org.json.simple.JSONValue;
 
 
 public class Comico {
 	public static void main(String[] args) throws UnsupportedEncodingException, IOException {
 		ArrayList<CrawWebtoonVO> list = new ArrayList<CrawWebtoonVO>();
-		getComico(list);
+		System.out.println(getComico(list));
 	}
 	
-	public static void getComico(ArrayList<CrawWebtoonVO> list) throws UnsupportedEncodingException, IOException{
+	public static String getComico(ArrayList<CrawWebtoonVO> list) throws UnsupportedEncodingException, IOException{
 		
-	//코미코 연재작
-	URL url = new URL("https://www.comico.kr/justoon/api/comic/serial/dayofweek");
-	
-	HttpURLConnection huc = (HttpURLConnection)url.openConnection();
-	huc.setRequestMethod("POST");
-	huc.connect();
-	InputStreamReader isr = new InputStreamReader(huc.getInputStream(), "UTF-8");
-	JSONObject object = (JSONObject) JSONValue.parse(isr);
-	
-	//CartoonVO 담을 배열 생성
-	
-	System.out.println(object);
-	
-	
+		 URL url;
+	        HttpURLConnection conn;
+	        BufferedReader br;
+	        BufferedWriter bw;
+	        String line;
+	        String result = "";
+	        String parameter = String.format("page=%s&code=%s&tabType=%s&days=%s",URLEncoder.encode("webtoon","UTF-8"),
+	        		URLEncoder.encode("days","UTF-8"),URLEncoder.encode("1","UTF-8"),URLEncoder.encode("127","UTF-8"));
+	 
+	        try {
+	            url = new URL("https://www.comico.kr/comic/home?"+parameter);
+	            conn = (HttpURLConnection) url.openConnection();          
+	            conn.setRequestMethod("GET");
+	            conn.setDoInput(true);
+	            conn.setDoOutput(true);
+	            conn.setInstanceFollowRedirects(false);
+	            conn.setRequestProperty("Content-Type", "application/json");
+	 
+	            //파라메터가 없으면 지워주면 된다.
+	            bw = new BufferedWriter(new OutputStreamWriter(conn.getOutputStream(), "UTF-8"));
+	            bw.write(parameter);
+	            //요기까지
+	 
+	            br = new BufferedReader(new InputStreamReader(conn.getInputStream(),"UTF-8"));
+	            while ((line = br.readLine()) != null)
+	                result += line + "\n";
+	            br.close();
+	             
+	        } catch (IOException e) {
+	            e.printStackTrace();
+	        } catch (Exception e) {
+	            e.printStackTrace();
+	        }
+	        return result;
 	}
 }
--- JSP_TeamProject_Team/src/com/allWebtoon/crawling/InsertWebtoon.java
+++ JSP_TeamProject_Team/src/com/allWebtoon/crawling/InsertWebtoon.java
@@ -33,9 +33,23 @@ public class InsertWebtoon {
 //			CrawWebtoonDAO.insWebtoonList(list.get(i));
 //		}
 		//4-2. 다음 웹툰 연재작 DB 담기(카카오와 중복 제거되서 총 161개 중 126개 삽입)
-		Daum.getCompleteDaum(list);;
-		for (int i = 0; i < list.size(); i++) {
-			CrawWebtoonDAO.insWebtoonList(list.get(i));
-		}
+//		Daum.getCompleteDaum(list);;
+//		for (int i = 0; i < list.size(); i++) {
+//			CrawWebtoonDAO.insWebtoonList(list.get(i));
+//		}
+		//삽입 과정에서 foreign key때문에 에러터지면서 중복제거 될거임
+		//5-1. 탑툰 웹툰 연재작 DB 담기(총 347개 -> 243개 삽입) 
+//		Toptoon.getToptoon(list);
+//		for (int i = 0; i < list.size(); i++) {
+//			CrawWebtoonDAO.insWebtoonList(list.get(i));
+//		}
+		//5-2. 탑툰 웹툰 연재작 DB 담기(총 197개 -> 131개 삽입)
+		//연재작 중 완결작도 끼어있음, 탑툰에서 연재중인 웹툰 중에 완결작도 끼어있어서 이럼....
+		//총 탑툰 374개(연재 + 완결)
+//		Toptoon.getCompleteToptoon(list);
+//		for (int i = 0; i < list.size(); i++) {
+//			CrawWebtoonDAO.insWebtoonList(list.get(i));
+//		}
+		//5-3. 데이터베이스 t_platform의 5번 코미코 -> 탑툰으로 변경
 	}
 }
