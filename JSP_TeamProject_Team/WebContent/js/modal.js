cff613765da789afaa23bfc836aa8f36646868ab
truespring <true_spring_@naver.com>
2020-09-28 19:18:42 +0900

    모달 & 모달에 댓글 뿌리기

--- JSP_TeamProject_Team/WebContent/js/modal.js
+++ JSP_TeamProject_Team/WebContent/js/modal.js
@@ -1,11 +1,12 @@
 /**
  * 
- */
+ 
 	const openButton = document.getElementById("open")
 	const modal = document.querySelector(".modal")
 	const overlay = modal.querySelector(".modal__overlay")
 	const closeBtn = modal.querySelector("button")
-	const openModal = () => {
+	function openModal(idx){
+		mySwiper.slideTo(idx)
 	    modal.classList.remove("hidden")
 	}
 	const closeModal = () => {
@@ -13,4 +14,7 @@
 	}
 	overlay.addEventListener("click", closeModal)
 	closeBtn.addEventListener("click", closeModal)
-	openButton.addEventListener("click", openModal)
+	openButton.addEventListener("click", function() {
+		openModal(idx + 1)
+	})
+	*/
