55ecbed3f0e2d03fa4b2cb79f9f0bb51fa93bcc3
tjs531 <tjs531@naver.com>
2020-10-02 15:14:36 +0900

    마이페이지 찜한웹툰 목록 추가

--- JSP_TeamProject_Team/src/com/allWebtoon/view/MyPageSer.java
+++ JSP_TeamProject_Team/src/com/allWebtoon/view/MyPageSer.java
@@ -23,12 +23,17 @@ public class MyPageSer extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		int idx = MyUtils.getIntParameter(request, "page");
+		String type = request.getParameter("type");
+		
+		System.out.println("type: " + type );
+		  
 		//로그인 체크
 		UserVO loginUser = MyUtils.getLoginUser(request);
 		if(loginUser == null) {
 			response.sendRedirect("/login");
 			return;
 		}
+
 		List<WebtoonCmtDomain> selRecentList = new ArrayList<WebtoonCmtDomain>();
 		MyPageDAO.selRecentlyWebtoon(selRecentList, loginUser.getU_no());
 		if(selRecentList.size() > 0) {
@@ -37,7 +42,34 @@ public class MyPageSer extends HttpServlet {
 		//댓글 및 평점 배열
 		List<WebtoonCmtDomain> selCmtList = new ArrayList<WebtoonCmtDomain>();
 		MyPageDAO.myWebtoon(selCmtList, loginUser.getU_no());
-		if(selCmtList.size() == 0 && idx == 0) {
+	
+		//찜한 웹툰  
+		List<WebtoonCmtDomain> favoriteList = new ArrayList<WebtoonCmtDomain>();
+		MyPageDAO.selfavoriteWebtoon(favoriteList, loginUser.getU_no());
+		
+		if(type == null) {
+
+			List<WebtoonCmtDomain> first_cmtList = new ArrayList<WebtoonCmtDomain>();
+			List<WebtoonCmtDomain> first_favoriteList = new ArrayList<WebtoonCmtDomain>();
+			
+			if(selCmtList.size() != 0) {
+				for (int i = 0; i < (selCmtList.size() > 5 ? 6 : selCmtList.size()); i++) {
+					first_cmtList.add(selCmtList.get(i));
+				}
+				request.setAttribute("list", first_cmtList);
+			}
+			if(favoriteList.size() != 0) {
+				for (int i = 0; i < (favoriteList.size() > 5 ? 6 : favoriteList.size()); i++) {
+					first_favoriteList.add(favoriteList.get(i));
+				}
+				request.setAttribute("favoritelist", first_favoriteList);
+			}
+			
+			ViewResolver.accessForward("myPage", request, response);
+		
+		}
+		
+	/*	if(selCmtList.size() == 0 && idx == 0) {
 			ViewResolver.accessForward("myPage", request, response);
 			return;
 		}else if(selCmtList.size() >= 1 && idx == 0) {
@@ -48,20 +80,29 @@ public class MyPageSer extends HttpServlet {
 			}
 			request.setAttribute("list", firstList);
 			
-			ViewResolver.accessForward("myPage", request, response);
+			ViewResolver.accessForward("myPage", request, response);*/
 		//ajax 로직
-		}else {
-			if(selCmtList.size() >= idx) {
+		//}
+		else {
+			List<WebtoonCmtDomain> list;
+			
+			if("cmt".equals(type)) {
+				list = selCmtList;
+			}else {
+				list = favoriteList;
+			}
+			
+			if(list.size() >= idx) {
 				idx -= 1;
 				System.out.println("ajax 왔음  ");
 				Gson gson = new Gson();
-				String json = gson.toJson(selCmtList.get(idx));
+				String json = gson.toJson(list.get(idx));
 				System.out.println("json : " + json);
 				response.setCharacterEncoding("UTF-8");
 				response.setContentType("application/json");
 				PrintWriter out = response.getWriter();
 				out.print(json);
-			}else if(selCmtList.size() == idx || selCmtList.size() < idx) {
+			}else if(list.size() == idx || list.size() < idx) {
 				PrintWriter out = response.getWriter();
 				out.print("0");
 			}
